aa46fddb5e24230de8b0169905e7865f410cbbe6
Wes Moberly <github.wes@mailhero.io>
2017-11-21 21:47:44 -0500

    Fix type in documentation for `argsToObj`

--- src/losant/args-to-obj.js
+++ src/losant/args-to-obj.js
@@ -10,7 +10,7 @@ const list = require('./list');
  * @signature [k1, k2, k3] -> (a, b, c, d) -> { k1: a, k2: b, k3: c }
  *
  * @example
- *     argsToObj(['foo', 'bar'])(1, 2); // { foo: 2, bar: 2 }
+ *     argsToObj(['foo', 'bar'])(1, 2); // { foo: 1, bar: 2 }
  *
  *     function foo() {
  *       return argsToObj(['a', 'b', 'c'])(arguments);
