8f81498b5fdad981abe92fbc1bbc9095263cdcc7
Wes Moberly <github.wes@mailhero.io>
2017-11-27 13:50:36 -0500

    Add docs for `fpThrow`

--- src/losant/fp-throw.js
+++ src/losant/fp-throw.js
@@ -1,9 +1,14 @@
 const { is } = require('ramda');
 
 // TODO: TESTS
-// TODO: DOCS
 
 /**
+ * Takes an error and immediately throws it. Useful for throwing an error when
+ * using pipe, compose, cond, etc. without having to create an anonymous
+ * function to throw it. If the given error is not a Error instance then it
+ * will be used as the error message and a new Error instance will be created
+ * and then thrown.
+ *
  * @signature Error | String -> void
  *
  * @throws {Error} The given error.
