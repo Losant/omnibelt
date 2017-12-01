f56e9721fa694d5e2e945edead67de849089ab04
Wes Moberly <github.wes@mailhero.io>
2017-12-01 12:43:53 -0500

    Add `thunkify`

---
+++ src/losant/thunkify.js
@@ -0,0 +1,15 @@
+/**
+ * Takes a function and returns a function that takes any number of arguments
+ * and returns a thunk that calls the original function with those arguments.
+ *
+ * @signature Function -> Function -> Function -> *
+ *
+ * @example
+ *    const logItems = pipe(list, join(', ') console.log);
+ *    const logger = thunkify(logItems);
+ *    const log123 = logger(1, 2, 3);
+ *    log123(); // => 1, 2, 3
+ */
+const thunkify = (fn) => (...args) => () => fn(...args);
+
+module.exports = thunkify;
