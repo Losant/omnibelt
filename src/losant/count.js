a89af81cf2a3aeb88d6674969049be3fa3a3b709
Rob Hilgefort <rjhilgefort@gmail.com>
2018-01-18 00:15:04 -0500

    Implement test to check for duplicate utils...
    - added `count`
    - added `intersectAny`

---
+++ src/losant/count.js
@@ -0,0 +1,16 @@
+const { countBy, identity } = require('ramda');
+
+/**
+ * `countBy` the raw values in an array (`identity`). All values are
+ * coerced to strings.
+ *
+ * @signature [a] → {*}
+ * @see `R.countBy`.
+ *
+ * @example
+ *   count([1, 1, 2]);           // { '1': 2, '2': 1 }
+ *   count(['a', 'b', 'a']);     // { a: 2, b: 1 }
+ */
+const count = countBy(identity);
+
+module.exports = count;
---
+++ src/losant/intersect-any.js
@@ -0,0 +1,35 @@
+const {
+  filter, gt, __, compose, unnest, keys, map,
+  unless, equals,
+} = require('ramda');
+const count = require('./count');
+
+const gt1 = gt(__, 1);
+
+// :: String -> String | Number
+const parseIntIfPossible =
+  unless(
+    compose(equals(NaN), parseInt),
+    parseInt,
+  );
+
+/**
+ * Finds *any* intersections between any number of lists.
+ *
+ * @signature [[String | Number a]] -> [a]
+ *
+ * @example
+ *   intersectAny([[1, 2, 3], [5, 6, 9], [3, 9]])            // => [3, 9]
+ *   intersectAny([['foo', 'bar'], ['baz'], ['bar', 'qux']]) // => ['bar']
+ */
+// TODO: Handle strings that look like numbers. See tests.
+const intersectAny =
+  compose(
+    map(parseIntIfPossible),
+    keys,
+    filter(gt1),
+    count,
+    unnest
+  );
+
+module.exports = intersectAny;
