3c3b30858b024f147e77a0ac4128a4ce548b85b3
Wes Moberly <github.wes@mailhero.io>
2018-02-02 13:32:34 -0500

    Add more test cases for `equalsShallow`

--- src/losant/equals-shallow.test.js
+++ src/losant/equals-shallow.test.js
@@ -3,10 +3,27 @@ const { pipe, map, apply, adjust } = require('ramda');
 const testHarness = require('./test-harness');
 const equalsShallow = require('./equals-shallow');
 
+const getTestDate = () => new Date(2017, 1, 1);
+
+class TestDataStructure {
+  constructor(label) {
+    this.label = label;
+  }
+}
+
 const cases = [
   [{ a: 1 }, { a: 1 }, true],
   [{ a: 1 }, { a: 2 }, false],
   [{ a: { b: 1 } }, { a: { b: 1 } }, false],
+  ['a', 'a', true],
+  [1, 1, true],
+  [[1, 2, 3], [1, 2, 3], true],
+  [getTestDate(), getTestDate(), true],
+  [new TestDataStructure('a'), new TestDataStructure('a'), true],
+  [new TestDataStructure('a'), new TestDataStructure('b'), false],
+  [null, null, true],
+  [undefined, undefined, true],
+  [null, undefined, false],
 ];
 
 const testHarnessAdapter = pipe(
