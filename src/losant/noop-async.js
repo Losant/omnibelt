5109d5b7bc1dd2d4556d928abe1167f2e57d6740
Michael Kuehl <michael@losant.com>
2018-02-05 11:19:54 -0500

    I've been conviced

--- src/losant/noop-async.js
+++ src/losant/noop-async.js
@@ -1,11 +1,8 @@
 /**
  * This is async version of noop.
  *
- * Explicitly went with async instead of Promise.resolve, since under the covers they might not
- * be identical, and the name noopAsync suggests "uses async"
- *
  * @signature -> Promise<undefined>
  */
-const noopAsync = async () => {};
+const noopAsync = () => { return Promise.resolve(undefined); };
 
 module.exports = noopAsync;
