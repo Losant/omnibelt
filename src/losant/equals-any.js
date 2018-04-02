94b21f7a5da28f1504b1956d22d57eb8d9e8d08a
Rob Hilgefort <rjhilgefort@gmail.com>
2018-04-02 16:01:02 -0400

    remove trailing space

--- src/losant/equals-any.js
+++ src/losant/equals-any.js
@@ -7,7 +7,7 @@ const contains = require('ramda/src/contains');
  * @signature Array<* a> -> a -> Boolean
  * @example
  *   equalsAny([1, 2, 3], 2); // => true
- *   equalsAny(['a', 'b', 'c'], 2); // => false 
+ *   equalsAny(['a', 'b', 'c'], 2); // => false
  */
 const equalsAny = flip(contains);
 
