baf04a7c6bf4ead3b6e6e0e776cf5b1032dc3c69
Rob Hilgefort <rjhilgefort@gmail.com>
2018-04-21 00:48:35 -0400

    generate with new lodash version in template

--- src/lodash/index-docs.js
+++ src/lodash/index-docs.js
@@ -3,257 +3,257 @@
  * @memberof module:lodash
  * @name camelCase
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#camelCase|_.camelCase}
+ * @see {@link https://lodash.com/docs/4.17.5#camelCase|_.camelCase}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name capitalize
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#capitalize|_.capitalize}
+ * @see {@link https://lodash.com/docs/4.17.5#capitalize|_.capitalize}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name castArray
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#castArray|_.castArray}
+ * @see {@link https://lodash.com/docs/4.17.5#castArray|_.castArray}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name compact
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#compact|_.compact}
+ * @see {@link https://lodash.com/docs/4.17.5#compact|_.compact}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name escape
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#escape|_.escape}
+ * @see {@link https://lodash.com/docs/4.17.5#escape|_.escape}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name escapeRegExp
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#escapeRegExp|_.escapeRegExp}
+ * @see {@link https://lodash.com/docs/4.17.5#escapeRegExp|_.escapeRegExp}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name flip
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#flip|_.flip}
+ * @see {@link https://lodash.com/docs/4.17.5#flip|_.flip}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name forEach
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#forEach|_.forEach}
+ * @see {@link https://lodash.com/docs/4.17.5#forEach|_.forEach}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name isArrayLike
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#isArrayLike|_.isArrayLike}
+ * @see {@link https://lodash.com/docs/4.17.5#isArrayLike|_.isArrayLike}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name isNull
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#isNull|_.isNull}
+ * @see {@link https://lodash.com/docs/4.17.5#isNull|_.isNull}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name isUndefined
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#isUndefined|_.isUndefined}
+ * @see {@link https://lodash.com/docs/4.17.5#isUndefined|_.isUndefined}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name kebabCase
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#kebabCase|_.kebabCase}
+ * @see {@link https://lodash.com/docs/4.17.5#kebabCase|_.kebabCase}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name keyBy
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#keyBy|_.keyBy}
+ * @see {@link https://lodash.com/docs/4.17.5#keyBy|_.keyBy}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name lowerFirst
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#lowerFirst|_.lowerFirst}
+ * @see {@link https://lodash.com/docs/4.17.5#lowerFirst|_.lowerFirst}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name mapKeys
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#mapKeys|_.mapKeys}
+ * @see {@link https://lodash.com/docs/4.17.5#mapKeys|_.mapKeys}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name noop
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#noop|_.noop}
+ * @see {@link https://lodash.com/docs/4.17.5#noop|_.noop}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name pad
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#pad|_.pad}
+ * @see {@link https://lodash.com/docs/4.17.5#pad|_.pad}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name padChars
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#padChars|_.padChars}
+ * @see {@link https://lodash.com/docs/4.17.5#padChars|_.padChars}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name padCharsEnd
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#padCharsEnd|_.padCharsEnd}
+ * @see {@link https://lodash.com/docs/4.17.5#padCharsEnd|_.padCharsEnd}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name padCharsStart
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#padCharsStart|_.padCharsStart}
+ * @see {@link https://lodash.com/docs/4.17.5#padCharsStart|_.padCharsStart}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name padEnd
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#padEnd|_.padEnd}
+ * @see {@link https://lodash.com/docs/4.17.5#padEnd|_.padEnd}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name padStart
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#padStart|_.padStart}
+ * @see {@link https://lodash.com/docs/4.17.5#padStart|_.padStart}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name snakeCase
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#snakeCase|_.snakeCase}
+ * @see {@link https://lodash.com/docs/4.17.5#snakeCase|_.snakeCase}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name split
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#split|_.split}
+ * @see {@link https://lodash.com/docs/4.17.5#split|_.split}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name startCase
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#startCase|_.startCase}
+ * @see {@link https://lodash.com/docs/4.17.5#startCase|_.startCase}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toInteger
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toInteger|_.toInteger}
+ * @see {@link https://lodash.com/docs/4.17.5#toInteger|_.toInteger}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toLower
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toLower|_.toLower}
+ * @see {@link https://lodash.com/docs/4.17.5#toLower|_.toLower}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toNumber
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toNumber|_.toNumber}
+ * @see {@link https://lodash.com/docs/4.17.5#toNumber|_.toNumber}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toPlainObject
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toPlainObject|_.toPlainObject}
+ * @see {@link https://lodash.com/docs/4.17.5#toPlainObject|_.toPlainObject}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toString
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toString|_.toString}
+ * @see {@link https://lodash.com/docs/4.17.5#toString|_.toString}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name toUpper
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#toUpper|_.toUpper}
+ * @see {@link https://lodash.com/docs/4.17.5#toUpper|_.toUpper}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name trim
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#trim|_.trim}
+ * @see {@link https://lodash.com/docs/4.17.5#trim|_.trim}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name trimEnd
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#trimEnd|_.trimEnd}
+ * @see {@link https://lodash.com/docs/4.17.5#trimEnd|_.trimEnd}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name trimStart
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#trimStart|_.trimStart}
+ * @see {@link https://lodash.com/docs/4.17.5#trimStart|_.trimStart}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name unescape
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#unescape|_.unescape}
+ * @see {@link https://lodash.com/docs/4.17.5#unescape|_.unescape}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name upperFirst
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#upperFirst|_.upperFirst}
+ * @see {@link https://lodash.com/docs/4.17.5#upperFirst|_.upperFirst}
  */
 /**
  * @func
  * @memberof module:lodash
  * @name words
  * @see {@link https://github.com/lodash/lodash/wiki/FP-Guide|FP-Guide For Actual Interface}
- * @see {@link https://lodash.com/docs/4.17.4#words|_.words}
+ * @see {@link https://lodash.com/docs/4.17.5#words|_.words}
  */
