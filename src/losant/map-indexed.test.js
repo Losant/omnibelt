f9233f2f8b5b7b7d69b76d81c8214fa6804f869d
Rob Hilgefort <rjhilgefort@gmail.com>
2018-05-02 08:53:46 -0400

    update docs

--- src/losant/map-indexed.test.js
+++ src/losant/map-indexed.test.js
@@ -7,7 +7,7 @@ testCases(mapIndexed, [
     ['a-0', 'b-1']
   ],
   // Used to demonstrate that this doesn't do what you might expect for objects.
-  // You should use `R.mapObjIndexed`.
+  // See: `R.mapObjIndexed`.
   [
     [(value, index) => `${value}-${index}`, { foo: 'a', bar: 'b' }],
     { foo: 'a-0', bar: 'b-1' }
