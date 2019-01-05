ac454ab5ad84e68564d2e52ea5b20c94486d0f10
Michael Kuehl <michael@losant.com>
2019-01-04 21:35:17 -0500

    clean these tests up a bit

--- src/omnibelt/serial-for-each-p.test.js
+++ src/omnibelt/serial-for-each-p.test.js
@@ -1,52 +1,29 @@
 const serialForEachP = require('./serial-for-each-p');
 const sleep = require('./sleep');
 
+const harness = async (input) => {
+  const output = [];
+  await serialForEachP(async (val) => {
+    await sleep(Math.random());
+    output.push(Array.isArray(val) ? val.join(', ') : val * 2);
+  }, input);
+  return output;
+};
+
 describe('serialForEachP', () => {
   it('should correctly iterate on an array, serially, in order', async () => {
-    const input = [1, 2, 3, 4, 5];
-    const output = [];
-
-    await serialForEachP(async (val) => {
-      await sleep(Math.random());
-      output.push(val*2);
-    }, input);
-
-    expect(output).toEqual([2, 4, 6, 8, 10]);
+    expect(await harness([1, 2, 3, 4, 5])).toEqual([2, 4, 6, 8, 10]);
   });
 
   it('should correctly iterate on a Set, serially, in order', async () => {
-    const input = new Set([1, 2, 3, 4, 5]);
-    const output = [];
-
-    await serialForEachP(async (val) => {
-      await sleep(Math.random());
-      output.push(val*2);
-    }, input);
-
-    expect(output).toEqual([2, 4, 6, 8, 10]);
+    expect(await harness(new Set([1, 2, 3, 4, 5]))).toEqual([2, 4, 6, 8, 10]);
   });
 
   it('should correctly iterate on a Map, serially, in order', async () => {
-    const input = new Map([[1, 2], [3, 4], [5, 6]]);
-    const output = [];
-
-    await serialForEachP(async ([k, v]) => {
-      await sleep(Math.random());
-      output.push(k + v);
-    }, input);
-
-    expect(output).toEqual([3, 7, 11]);
+    expect(await harness(new Map([[1, 2], [3, 4], [5, 6]]))).toEqual(['1, 2', '3, 4', '5, 6']);
   });
 
   it('should correctly iterate on an Object, serially, in order', async () => {
-    const input = { a: 2, b: 4, c: 6 };
-    const output = [];
-
-    await serialForEachP(async ([k, v]) => {
-      await sleep(Math.random());
-      output.push(k + v);
-    }, input);
-
-    expect(output).toEqual(['a2', 'b4', 'c6']);
+    expect(await harness( { a: 2, b: 4, c: 6 })).toEqual(['a, 2', 'b, 4', 'c, 6']);
   });
 });
