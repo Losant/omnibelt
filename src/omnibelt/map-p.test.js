00e9e6abee6d438258a516bae9882fd9008ad06f
eeerin <erin@losant.com>
2021-05-14 11:34:55 -0400

    added more tests for map-p

--- src/omnibelt/map-p.test.js
+++ src/omnibelt/map-p.test.js
@@ -21,3 +21,20 @@ it('Works with an async iterator on Objects', () => {
 
   return expect(actual).resolves.toEqual(expected);
 });
+
+it('Will wait for all promises to complete before throwing an error', async () => {
+  let callCount = 0;
+  let err;
+  await mapP((x) => {
+    return new Promise((resolve, reject) => {
+      callCount++;
+      if (x >= 5) {
+        return reject(new Error(`Error ${x}`));
+      }
+      return resolve(x + 1);
+    });
+  }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).catch((e) => { err = e; });
+
+  expect(callCount).toEqual(10);
+  expect(err.message).toEqual('Error 5');
+});
