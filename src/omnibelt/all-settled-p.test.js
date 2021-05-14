198a9a433e763e0b5f529d7e466438c07af1299d
eeerin <erin@losant.com>
2021-05-14 10:52:15 -0400

    adding another allSettledP test

--- src/omnibelt/all-settled-p.test.js
+++ src/omnibelt/all-settled-p.test.js
@@ -7,4 +7,16 @@ describe('allSettledP', () => {
     const output = await allSettledP(input);
     expect(output).toEqual([{ state: 'fulfilled', value: 1 }, { state: 'rejected', reason: err }]);
   });
+  it('should correctly settle the promises', async () => {
+    const err = new Error('error');
+    const input = {
+      a: Promise.resolve(1),
+      b: Promise.reject(err)
+    };
+    const output = await allSettledP(input);
+    expect(output).toEqual({
+      a: { state: 'fulfilled', value: 1 },
+      b: { state: 'rejected', reason: err }
+    });
+  });
 });
